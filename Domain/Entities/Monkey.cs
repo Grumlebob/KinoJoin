8cabe7287b6c5e2e2a1ae715432922688af781ad
grumlebob <grumlebob@gmail.com>
2024-02-16 14:58:07 +0100

    Testing CSharpier violation

--- Domain/Entities/Monkey.cs
+++ Domain/Entities/Monkey.cs
@@ -7,8 +7,6 @@ public class Monkey
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
-
-    public required string Name { get; set; }
+    public int Id { get; set; } public required string Name { get; set; }
     public required int Age { get; set; }
 }

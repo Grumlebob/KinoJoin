e30efe7c92d1d81d1981ffa8b8a4e63180a78edd
grumlebob <grumlebob@gmail.com>
2024-02-16 15:04:20 +0100

    csharpier formatting

--- Domain/Entities/Monkey.cs
+++ Domain/Entities/Monkey.cs
@@ -7,6 +7,7 @@ public class Monkey
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; } public required string Name { get; set; }
+    public int Id { get; set; }
+    public required string Name { get; set; }
     public required int Age { get; set; }
 }

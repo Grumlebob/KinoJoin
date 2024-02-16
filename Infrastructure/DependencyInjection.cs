c8712b7e57beab5a8c510b0dcacbee6753099614
grumlebob <grumlebob@gmail.com>
2024-02-16 12:17:10 +0100

    Connectionstring

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
     {
         services.AddDbContextFactory<DataContext>(options =>
         {
-            var secret = builder.Configuration["PostgresConnection"];
+            var secret = configuration["PostgresConnection"];
             options.UseNpgsql(secret);
             options.EnableDetailedErrors();
         });

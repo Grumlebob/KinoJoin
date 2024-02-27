f36e1e1a75616b02b43354c266585cf6f2dea929
grumlebob <grumlebob@gmail.com>
2024-02-27 09:31:06 +0100

    Added tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:
+      [
+        './**/*.razor',//it will scan all razor files in the project and will find all the tailwind classes that you use
+        './wwwroot/index.html' //not necessary if you use Blazor 8 Web App (not just standalone wasm app)
+      ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

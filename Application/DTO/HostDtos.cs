95f314c4219b272fc84f8163616c7637c424eccd
grumlebob <grumlebob@gmail.com>
2024-02-28 08:55:05 +0100

    Componenetified event details

---
+++ Application/DTO/HostDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTO;
+
+//Not a record, because we want mutability when changing the details.
+public class HostJoinEventDetails
+{
+    public string? EventDescription { get; set; }
+    public string? EventTitle { get; set; }
+    public DateTime Deadline { get; set; }
+}

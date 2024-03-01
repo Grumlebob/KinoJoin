2e79f03ee1fe3c70e0de8c869bad01ba008f8f8c
alberted <alberted115@gmail.com>
2024-03-01 11:01:46 +0100

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,37 @@ module.exports = {
   theme: {
       fontFamily:{
           'sans': ['basier-square']
-      }
-    extend: {},
+      },
+    extend: {
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            primary: {
+                DEFAULT: "#e30613",
+                dark: "#cf1d1f",
+                darkest: "#a91719",
+            },
+            secondary: {
+                DEFAULT: "#141414",
+            },
+            success: {
+                DEFAULT: "rgb(101,193,121)",
+                dark: "rgb(92,174,109)",
+                darkest: "rgb(81,154,97)",
+            },
+            warning: {
+                DEFAULT: "rgb(253,185,0)",
+                dark: "rgb(220,159,0)",
+                darkest: "rgb(200,146,0)",
+            },
+            error: {
+                DEFAULT: "rgb(226,117,120)",
+                dark: "rgb(203,105,109)",
+                darkest: "rgb(182 93 96)",
+            },
+        },
+          
+    },
   },
   plugins: [],
 }

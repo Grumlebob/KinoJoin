3c5d3785a3c4f22b65e40f75599ed460467303b8
ThomasBavn <102732812+ThomasBavn@users.noreply.github.com>
2024-03-01 11:10:26 +0100

    Import fonts used on Kino.dk (#31)
    * Add fonts
    
    * Normalize font family
    
    * Add new icon
    
    * Apply basier globally
    
    * csharpier
    
    * update gitattributes
    
    * Update gitattributes locally
    
    * add DS_store to gitignore
    
    * update gitattributes
    
    * disable gitattributes
    
    * Add colors
    
    ---------
    
    Co-authored-by: alberted <alberted115@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,39 @@ module.exports = {
         './Presentation/Presentation.Client/Pages/**/*.razor',
       ],
   theme: {
-    extend: {},
+      fontFamily:{
+          'sans': ['basier-square']
+      },
+    extend: {
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            primary: {
+                DEFAULT: "#e30613",
+                dark: "#cf1d1f",
+                darkest: "#a91719",
+            },
+            secondary: {
+                DEFAULT: "#141414",
+            },
+            success: {
+                DEFAULT: "rgb(101,193,121)",
+                dark: "rgb(92,174,109)",
+                darkest: "rgb(81,154,97)",
+            },
+            warning: {
+                DEFAULT: "rgb(253,185,0)",
+                dark: "rgb(220,159,0)",
+                darkest: "rgb(200,146,0)",
+            },
+            error: {
+                DEFAULT: "rgb(226,117,120)",
+                dark: "rgb(203,105,109)",
+                darkest: "rgb(182 93 96)",
+            },
+        },
+          
+    },
   },
   plugins: [],
 }
-

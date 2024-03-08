100a439a43c4a01b8e6c5aec3419a5e4c9a957a3
grumlebob <grumlebob@gmail.com>
2024-03-08 15:18:45 +0100

    De-duplicated code, moved into private helper method

--- Infrastructure/Services/JoinEventService.cs
+++ Infrastructure/Services/JoinEventService.cs
@@ -8,7 +8,7 @@ public class JoinEventService(KinoContext context) : IJoinEventService
     {
         var joinEventWithNavProps = UpsertJoinEventDto.FromUpsertDtoToModel(joinEventDto);
 
-        //make sure all entities exist in database
+        //Upsert SelectOptions, Playtimes, Rooms and host: To make sure they exist in DB before JoinEvent is added
         await context.Hosts.Upsert(joinEventWithNavProps.Host).RunAsync();
 
         await context
@@ -26,131 +26,126 @@ public class JoinEventService(KinoContext context) : IJoinEventService
             .DistinctBy(r => r.Id)
             .ToList();
         await context.Rooms.UpsertRange(newRooms).RunAsync();
-
         await context.SaveChangesAsync();
 
         //Try to add JoinEvent using preseeded data in database
         try
         {
-            foreach (var showtime in joinEventWithNavProps.Showtimes)
-            {
-                showtime.PlaytimeId = (
-                    await context.Playtimes.FirstAsync(p =>
-                        p.StartTime == showtime.Playtime.StartTime
-                    )
-                ).Id;
-                showtime.VersionTagId = (
-                    await context.Versions.FirstAsync(v => v.Type == showtime.VersionTag.Type)
-                ).Id;
-            }
-
-            await context.Showtimes.UpsertRange(joinEventWithNavProps.Showtimes).RunAsync();
-            await context.SaveChangesAsync();
-
-            joinEventWithNavProps.Host = null!; // avoid creating a new host
-
-            //As mulitple showtimes can reference the same movie etc, we must only attach it once in ef core
-            var showtimeIds = joinEventDto.Showtimes.Select(s => s.Id);
-            var voteOptions = joinEventDto.SelectOptions.Select(s => s.VoteOption);
-            var colorOptions = joinEventDto.SelectOptions.Select(s => s.Color);
-
-            EntityEntry<JoinEvent> newlyUpsertedJoinEvent; //is set by add or update
-            if (joinEventDto.Id == null) //new event
-            {
-                //use existing showtimes etc
-                joinEventWithNavProps.Showtimes = context
-                    .Showtimes.Where(s => showtimeIds.Contains(s.Id))
-                    .ToList();
-                joinEventWithNavProps.SelectOptions = context
-                    .SelectOptions.Where(s =>
-                        voteOptions.Contains(s.VoteOption) && colorOptions.Contains(s.Color)
-                    )
-                    .ToList();
-                newlyUpsertedJoinEvent = await context.JoinEvents.AddAsync(joinEventWithNavProps);
-            }
-            else
-            {
-                //dont add already existing entities
-                joinEventWithNavProps.Participants.RemoveAll(p => p.Id != 0); //they are 0 if yet to be added to database
-                joinEventWithNavProps.SelectOptions.RemoveAll(s =>
-                    voteOptions.Contains(s.VoteOption) && colorOptions.Contains(s.Color)
-                );
-                joinEventWithNavProps.Showtimes = []; //these cannot change after joinEvent is created
-
-                newlyUpsertedJoinEvent = context.JoinEvents.Update(joinEventWithNavProps);
-            }
-
-            await context.SaveChangesAsync();
-            return newlyUpsertedJoinEvent.Entity.Id;
+            return await HandleJoinEventUpsert(joinEventDto, joinEventWithNavProps);
         }
-        catch (Exception _) //If not all data was found in the preseeded database, add it and try again
+        catch (Exception _)
         {
-            var movies = joinEventWithNavProps
-                .Showtimes.Select(st => st.Movie)
-                .DistinctBy(m => m.Id);
-            await context.Movies.UpsertRange(movies).RunAsync();
+            await UpsertMissingEntities(joinEventWithNavProps);
+            return await HandleJoinEventUpsert(joinEventDto, joinEventWithNavProps);
+        }
+    }
 
-            var cinemas = joinEventWithNavProps
-                .Showtimes.Select(st => st.Cinema)
-                .DistinctBy(c => c.Id);
-            await context.Cinemas.UpsertRange(cinemas).RunAsync();
+    private async Task<int> HandleJoinEventUpsert(
+        UpsertJoinEventDto joinEventDto,
+        JoinEvent joinEventWithNavProps
+    )
+    {
+        await AttachPlaytimeAndVersionId(joinEventWithNavProps.Showtimes);
 
-            var versions = joinEventWithNavProps
-                .Showtimes.Select(st => st.VersionTag)
-                .DistinctBy(v => v.Type);
-            await context.Versions.UpsertRange(versions).On(v => v.Type).RunAsync();
+        await context.Showtimes.UpsertRange(joinEventWithNavProps.Showtimes).RunAsync();
+        await context.SaveChangesAsync();
 
-            foreach (var showtime in joinEventWithNavProps.Showtimes)
+        joinEventWithNavProps.Host = null!; // Avoid creating a new host
+
+        bool isUpdate = joinEventDto.Id != null;
+        await PrepareJoinEvent(joinEventDto, joinEventWithNavProps, isUpdate);
+
+        EntityEntry<JoinEvent> newlyUpsertedJoinEvent = isUpdate
+            ? context.JoinEvents.Update(joinEventWithNavProps)
+            : await context.JoinEvents.AddAsync(joinEventWithNavProps);
+
+        await context.SaveChangesAsync();
+        return newlyUpsertedJoinEvent.Entity.Id;
+    }
+
+    private async Task PrepareJoinEvent(
+        UpsertJoinEventDto joinEventDto,
+        JoinEvent joinEventWithNavProps,
+        bool isUpdate
+    )
+    {
+        var voteOptions = joinEventDto.SelectOptions.Select(s => s.VoteOption);
+        var colorOptions = joinEventDto.SelectOptions.Select(s => s.Color);
+
+        if (isUpdate)
+        {
+            // Logic for updating JoinEvent
+            joinEventWithNavProps.Participants.RemoveAll(p => p.Id != 0); // They are 0 if yet to be added to database
+            joinEventWithNavProps.SelectOptions.RemoveAll(s =>
+                voteOptions.Contains(s.VoteOption) && colorOptions.Contains(s.Color)
+            );
+            joinEventWithNavProps.Showtimes = []; // These cannot change after joinEvent is created
+        }
+        else
+        {
+            var showtimeIds = joinEventDto.Showtimes.Select(s => s.Id);
+            // Logic for adding new JoinEvent
+            joinEventWithNavProps.Showtimes = context
+                .Showtimes.Where(s => showtimeIds.Contains(s.Id))
+                .ToList();
+            joinEventWithNavProps.SelectOptions = context
+                .SelectOptions.Where(s =>
+                    voteOptions.Contains(s.VoteOption) && colorOptions.Contains(s.Color)
+                )
+                .ToList();
+        }
+    }
+
+    private async Task UpsertMissingEntities(JoinEvent joinEventWithNavProps)
+    {
+        var movies = joinEventWithNavProps.Showtimes.Select(st => st.Movie).DistinctBy(m => m.Id);
+        await context.Movies.UpsertRange(movies).RunAsync();
+
+        var cinemas = joinEventWithNavProps.Showtimes.Select(st => st.Cinema).DistinctBy(c => c.Id);
+        await context.Cinemas.UpsertRange(cinemas).RunAsync();
+
+        var versions = joinEventWithNavProps
+            .Showtimes.Select(st => st.VersionTag)
+            .DistinctBy(v => v.Type);
+        await context.Versions.UpsertRange(versions).On(v => v.Type).RunAsync();
+
+        await AttachPlaytimeAndVersionId(joinEventWithNavProps.Showtimes);
+
+        await context.Showtimes.UpsertRange(joinEventWithNavProps.Showtimes).RunAsync();
+        await context.SaveChangesAsync();
+    }
+
+    private async Task AttachPlaytimeAndVersionId(List<Showtime> showtimes)
+    {
+        // Retrieve all distinct StartTimes and Types from the showtimes.
+        var distinctPlaytimes = showtimes.Select(st => st.Playtime.StartTime).Distinct();
+        var distinctVersionTypes = showtimes.Select(st => st.VersionTag.Type).Distinct();
+
+        // Retrieve all matching Playtimes and VersionTags from the database.
+        var playtimes = await context
+            .Playtimes.Where(p => distinctPlaytimes.Contains(p.StartTime))
+            .ToListAsync();
+        var versions = await context
+            .Versions.Where(v => distinctVersionTypes.Contains(v.Type))
+            .ToListAsync();
+
+        var versionDict = versions.ToDictionary(v => v.Type, v => v.Id);
+        var playtimeDict = playtimes.ToDictionary(p => p.StartTime.ToOADate(), p => p.Id);
+
+        foreach (var showtime in showtimes)
+        {
+            var LookupKey = showtime.Playtime.StartTime.ToOADate();
+
+            // Assign PlaytimeId and VersionTagId using the dictionaries.
+            if (versionDict.TryGetValue(showtime.VersionTag.Type, out var versionTagId))
             {
-                showtime.PlaytimeId = (
-                    await context.Playtimes.FirstAsync(p =>
-                        p.StartTime == showtime.Playtime.StartTime
-                    )
-                ).Id;
-                showtime.VersionTagId = (
-                    await context.Versions.FirstAsync(v => v.Type == showtime.VersionTag.Type)
-                ).Id;
+                showtime.VersionTagId = versionTagId;
             }
 
-            await context.Showtimes.UpsertRange(joinEventWithNavProps.Showtimes).RunAsync();
-            await context.SaveChangesAsync();
-
-            joinEventWithNavProps.Host = null!; // avoid creating a new host
-            EntityEntry<JoinEvent> newlyUpsertedJoinEvent; //is set by add or update
-            var voteOptions = joinEventDto.SelectOptions.Select(s => s.VoteOption);
-            var colorOptions = joinEventDto.SelectOptions.Select(s => s.Color);
-            if (joinEventDto.Id == null) //new event
+            if (playtimeDict.TryGetValue(LookupKey, out var playtimeId))
             {
-                //As mulitple showtimes can reference the same movie etc, we must only attach it once in ef core
-                var showtimeIds = joinEventDto.Showtimes.Select(s => s.Id);
-
-                //use existing showtimes etc
-                joinEventWithNavProps.Showtimes = context
-                    .Showtimes.Where(s => showtimeIds.Contains(s.Id))
-                    .ToList();
-                joinEventWithNavProps.SelectOptions = context
-                    .SelectOptions.Where(s =>
-                        voteOptions.Contains(s.VoteOption) && colorOptions.Contains(s.Color)
-                    )
-                    .ToList();
-                newlyUpsertedJoinEvent = await context.JoinEvents.AddAsync(joinEventWithNavProps);
+                showtime.PlaytimeId = playtimeId;
             }
-            else
-            {
-                //dont add already existing entities
-                joinEventWithNavProps.Participants.RemoveAll(p => p.Id != 0); //they are null if yet to be added to database
-                joinEventWithNavProps.SelectOptions = context
-                    .SelectOptions.Where(s =>
-                        voteOptions.Contains(s.VoteOption) && colorOptions.Contains(s.Color)
-                    )
-                    .ToList();
-                joinEventWithNavProps.Showtimes = []; //these cannot change after joinEvent is created
-
-                newlyUpsertedJoinEvent = context.JoinEvents.Update(joinEventWithNavProps);
-            }
-
-            await context.SaveChangesAsync();
-            return newlyUpsertedJoinEvent.Entity.Id;
         }
     }
 

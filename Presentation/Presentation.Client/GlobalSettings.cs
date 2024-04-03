fff4abe8a379223ecf414281c88fc38f8c88a3c2
grumlebob <grumlebob@gmail.com>
2024-04-03 10:44:21 +0200

    Custom javascript cleanup

--- Presentation/Presentation.Client/GlobalSettings.cs
+++ Presentation/Presentation.Client/GlobalSettings.cs
@@ -2,5 +2,11 @@ namespace Presentation.Client;
 
 public static class GlobalSettings
 {
-    public static bool HostOnNgrokWithNoHttpsAndSetDefaultUser { get; set; } = false;
+    /*
+     * This is used when we are hosting the application on ngrok without https.
+     * This is useful when we want to test the application on a mobile device.
+     * Because we have no certificates, we need to disable some login features, https redirection,
+     * and set a default user, to be able to test the application.
+     */
+    public const bool HostOnNgrokWithNoHttpsAndSetDefaultUser = false;
 }
--- Presentation/Presentation/Program.cs
+++ Presentation/Presentation/Program.cs
@@ -1,14 +1,6 @@
 using Application;
-using Application.Interfaces;
 using Carter;
-using Domain.Entities;
-using Domain.ExternalApiModels;
 using Infrastructure;
-using Infrastructure.Persistence;
-using Infrastructure.Services;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using Presentation;
 using Presentation.Client;
 using Presentation.Components;
 using _Imports = Presentation.Client._Imports;
@@ -20,18 +12,16 @@ builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// Add CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(
         "AllowAllOrigins",
-        builder =>
+        corsPolicyBuilder =>
         {
-            builder
-                .AllowAnyOrigin() // This allows requests from any origin
+            corsPolicyBuilder
+                .AllowAnyOrigin()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
-            // Do not call AllowCredentials() when using AllowAnyOrigin()
         }
     );
 });
@@ -69,4 +59,4 @@ app.MapCarter();
 app.Run();
 
 //A hacky solution to use Testcontainers with WebApplication.CreateBuilder for integration tests
-public partial class Program { }
+public partial class Program;

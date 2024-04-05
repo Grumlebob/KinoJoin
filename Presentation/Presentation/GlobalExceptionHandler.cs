a240efe70e2e41dd2088df24da9b6fe049749560
grumlebob <grumlebob@gmail.com>
2024-04-05 08:43:56 +0200

    Global exception handling

---
+++ Presentation/Presentation/GlobalExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Presentation;
+
+//The main idea is to not have a lot of try catch blocks inside the endpoints doing the same job.
+//Instead we propogate the exception up to this middleware, which will catch it and log it, and let the user know that something went wrong.
+public class GlobalExceptionHandler : IMiddleware
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) => _logger = logger;
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            ProblemDetails problem =
+                new()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Type = "Server error",
+                    Title = "Server error",
+                    Detail = "An internal server has occurred"
+                };
+
+            var json = JsonConvert.SerializeObject(problem);
+
+            context.Response.ContentType = "application/json";
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
